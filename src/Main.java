6e5fc9fdb852c338bd4c020b1f6b2bdf4762e301
Alex <sasha-ushakov@mail.ru>
2017-11-04 16:10:18 +0300

    Refactoring of Order class

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,7 @@ import jade.Boot;
 
 public class Main {
 
-
 	public static void main(String[] args) {
-
-		System.out.println("ok");
-
 
 		String[] parameters = new String[2];
 		parameters[0] = "-gui";
@@ -14,9 +10,9 @@ public class Main {
 				+ "AgentFinances:basicAgents.Finances;" + "AgentCapitalMarket:basicAgents.CapitalMarket;"
 				+ "AgentProcurement:basicAgents.Procurement;" + "AgentProcurementMarket:basicAgents.ProcurementMarket;"
 				+ "AgentProduction:basicAgents.Production;" + "sniffer:jade.tools.sniffer.Sniffer(Agent*);";
-		// AgentSalesMarket, AgentSelling, AgentFinances, AgentCapitalMarket, AgentProcurement, AgentProcurementMarket, AgentProduction
+		// AgentSalesMarket, AgentSelling, AgentFinances, AgentCapitalMarket,
+		// AgentProcurement, AgentProcurementMarket, AgentProduction
 		Boot.main(parameters);
-
 
 	}
 }
---
+++ src/basicClasses/OrderPart.java
@@ -0,0 +1,10 @@
+package basicClasses;
+
+public class OrderPart {
+	
+	public Product product;
+	public int amount;
+
+	public OrderPart() {
+	}
+}
