622103dbcae1d6ca6a778fffb5985547d2ea8da8
Henrik <henrikthienth@gmail.com>
2017-12-06 14:35:57 +0100

    updated server code, added client connection removed ChatObject class (Not needed)

---
+++ src/communication/MessageObject.java
@@ -0,0 +1,22 @@
+package communication;
+
+public class MessageObject {
+    private String message;
+
+    public MessageObject() {
+
+    }
+
+    public MessageObject(String message) {
+        super();
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- src/communication/Server.java
+++ src/communication/Server.java
@@ -4,11 +4,17 @@ import com.corundumstudio.socketio.AckRequest;
 import com.corundumstudio.socketio.Configuration;
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.listener.ConnectListener;
 import com.corundumstudio.socketio.listener.DataListener;
+import com.corundumstudio.socketio.listener.DisconnectListener;
 
 public class Server implements Runnable {
     private final String server = "localhost";
     private final int port = 9092;
+
+    private SocketIOServer conServer;
+    private SocketIOClient conClient;
+    private int connectionCounter;
 
     @Override
     /*
@@ -16,21 +22,60 @@ public class Server implements Runnable {
      */
     public void run() {
         System.out.println("SERVER STARTED ON PORT: " + port);
+        connectionCounter = 0;
 
-        final SocketIOServer server = new SocketIOServer(getConfig());
-        server.addEventListener("chatevent", ChatObject.class, new DataListener<ChatObject>() {
+        conServer = new SocketIOServer(getConfig());
+
+        /*
+            get all incoming connections to the server
+            allow only one connection
+         */
+        conServer.addConnectListener(new ConnectListener() {
             @Override
-            public void onData(SocketIOClient socketIOClient, ChatObject data, AckRequest ackRequest) throws Exception {
-                System.out.println("onData receive");
-                server.getBroadcastOperations().sendEvent("chatevent", data);
+            public void onConnect(SocketIOClient socketIOClient) {
+                // allow only one client connection
+                if (connectionCounter >= 1)
+                    socketIOClient.disconnect();
+
+                // count the connections
+                connectionCounter++;
+
+                // set the client
+                conClient = socketIOClient;
+            }
+        });
+        /*
+            handle all disconnects
+         */
+        conServer.addDisconnectListener(new DisconnectListener() {
+            @Override
+            public void onDisconnect(SocketIOClient socketIOClient) {
+                // count connections
+                connectionCounter--;
             }
         });
 
-        server.start();
+        /*
+            receiving messages from the client
+         */
+        conServer.addEventListener("msgevent", MessageObject.class, new DataListener<MessageObject>() {
+            @Override
+            public void onData(SocketIOClient socketIOClient, MessageObject messageObject, AckRequest ackRequest) throws Exception {
+
+            }
+        });
+
+        conServer.start();
     }
 
-    public void sendDataToClient() {
+    public void sendDataToClient(String event, MessageObject object) {
+        if (conClient != null)
+            conClient.sendEvent(event, object);
+    }
 
+    public void sendMessageToClient(String message) {
+        if (conClient != null)
+            conClient.sendEvent("msgevent", new MessageObject(message));
     }
 
     /*
@@ -44,6 +89,10 @@ public class Server implements Runnable {
     }
 
     private void stopServer() {
+        if (conClient != null)
+            conClient.disconnect();
 
+        if (conServer != null)
+            conServer.stop();
     }
 }
