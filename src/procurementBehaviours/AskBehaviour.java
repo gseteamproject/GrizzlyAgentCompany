2cb0a109be0d4cf2a4a8e286a033efb2d4be8633
Maxim Genko <gen-maksim@mail.ru>
2017-12-14 12:56:59 +0100

    implementing new outputs

--- src/procurementBehaviours/AskBehaviour.java
+++ src/procurementBehaviours/AskBehaviour.java
@@ -1,6 +1,7 @@
 package procurementBehaviours;
 
 import basicClasses.Order;
+import communication.MessageObject;
 import jade.core.behaviours.DataStore;
 import jade.core.behaviours.SimpleBehaviour;
 import jade.lang.acl.ACLMessage;
@@ -14,6 +15,7 @@ public class AskBehaviour extends SimpleBehaviour {
     private DataStore dataStore;
     private ProcurementResponder interactionBehaviour;
     Work interactor;
+    private MessageObject msgObj;
 
     public AskBehaviour(ProcurementResponder interactionBehaviour, DataStore dataStore) {
         this.interactionBehaviour = interactionBehaviour;
@@ -26,12 +28,20 @@ public class AskBehaviour extends SimpleBehaviour {
         ACLMessage request = interactionBehaviour.getRequest();
         String orderText = Order.gson.fromJson(request.getContent(), Order.class).getTextOfOrder();
         if (request.getConversationId() == "Materials") {
-            System.out.println("ProcurementAgent: [request] ProductionAgent asks for materials for " + orderText);
+            
+            msgObj = new MessageObject(request, orderText);
+            System.out.println(msgObj.getReceivedMessage());
+            
+//            System.out.println("ProcurementAgent: [request] ProductionAgent asks for materials for " + orderText);
             System.out.println("ProcurementAgent: [agree] I will check materialStorage for materials for " + orderText);
             myAgent.addBehaviour(new CheckMaterialStorage(myAgent, request));
         } else if (request.getConversationId() == "Take") {
-            System.out.println("ProcurementAgent: [request] ProductionAgent wants to get materials for " + orderText
-                    + " from materialStorage");
+            
+            msgObj = new MessageObject(request, orderText);
+            System.out.println(msgObj.getReceivedMessage());
+            
+//            System.out.println("ProcurementAgent: [request] ProductionAgent wants to get materials for " + orderText
+//                    + " from materialStorage");
             System.out.println(
                     "ProcurementAgent: [agree] I will give you materials for " + orderText + " from materialStorage");
             myAgent.addBehaviour(new GiveMaterialToProduction(myAgent, request));
