8da088546f369b77d90be5ba98e4f1555860c4fa
anwalt <andreas.walter1993@googlemail,com>
2018-02-07 19:15:35 +0100

    Communication GuiDelay added

--- src/communication/Communication.java
+++ src/communication/Communication.java
@@ -2,11 +2,19 @@ package communication;
 
 public class Communication {
     public static Server server;
+    public static long delaytime=5;
 
     public Communication() {
         server = new Server();
 
         Thread thread = new Thread(server);
         thread.start();
+    }
+    public void GUIdelay () throws InterruptedException {
+        try {
+            Thread.sleep(delaytime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
